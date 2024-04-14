47c6a23cdb4ad4634f0e7b00e64277fafd18e455
HUY <takasugi.tut@gmail.com>
2024-04-14 11:44:49 +0200

    add logger

--- svm_classifier.py
+++ svm_classifier.py
@@ -18,6 +18,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
+from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
+
+from log import Logger
 
 class SVMClassifier:
     def __init__(self, args):
@@ -31,8 +34,11 @@ class SVMClassifier:
         self.data = pd.read_csv(self.data_path)
         self.scaler = self.init_scaler(self.scaler_type)
         self.X_train, self.X_test, self.y_train, self.y_test = self.preprocessing_data()
+        
+        self.logger = Logger(self.output_dir)
 
-    def label_encode_categorical(self, data):
+    @staticmethod
+    def label_encode_categorical(data):
         """
         Converts categorical data into numerical form using LabelEncoder. 
         This is necessary because machine learning algorithms typically work with numerical data.
@@ -50,6 +56,41 @@ class SVMClassifier:
                 data[col]=le.fit_transform(data[col])
         return data
 
+    @staticmethod
+    def init_scaler(scaler):
+        """
+        Initializes the scaler based on the provided type. 
+
+        Args:
+            scaler (str): The type of scaler to use. Options are 'standard', 'maxmin', and 'robust'.
+
+        Returns:
+            Scaler: The initialized scaler. Default is StandardScaler if the provided type is not recognized.
+        """
+        scaler_map = {
+            'standard': StandardScaler(),
+            'maxmin': MinMaxScaler(),
+            'robust': RobustScaler(),
+        }
+        return scaler_map.get(scaler, StandardScaler())
+    
+    @staticmethod
+    def tune_hyperparameter():
+        """
+        Tunes the hyperparameters of the SVM using GridSearchCV. 
+
+        Returns:
+            GridSearchCV: The GridSearchCV object after fitting. This object can be used to access the best parameters found.
+        """
+        param_grid = {
+            'C': np.linspace(2 ** -5, 2 ** 15, 4),
+            'kernel': ['rbf'],
+            'gamma': np.linspace(2 ** -15, 2 ** 3, 4)
+        }
+        model_clf = svm.SVC()
+        grid = GridSearchCV(model_clf, param_grid, refit = True, verbose = 3)
+        return grid
+    
     def preprocessing_data(self):
         """
         Pre-processes the data by removing duplicates, encoding categorical variables, handling missing values, 
@@ -79,39 +120,6 @@ class SVMClassifier:
         
         return X_train, X_test, y_train, y_test
 
-    def init_scaler(self, scaler):
-        """
-        Initializes the scaler based on the provided type. 
-
-        Args:
-            scaler (str): The type of scaler to use. Options are 'standard', 'maxmin', and 'robust'.
-
-        Returns:
-            Scaler: The initialized scaler. Default is StandardScaler if the provided type is not recognized.
-        """
-        scaler_map = {
-            'standard': StandardScaler(),
-            'maxmin': MinMaxScaler(),
-            'robust': RobustScaler(),
-        }
-        return scaler_map.get(scaler, StandardScaler())
-
-    def tune_hyperparameter(self):
-        """
-        Tunes the hyperparameters of the SVM using GridSearchCV. 
-
-        Returns:
-            GridSearchCV: The GridSearchCV object after fitting. This object can be used to access the best parameters found.
-        """
-        param_grid = {
-            'C': np.linspace(2 ** -5, 2 ** 15, 4),
-            'kernel': ['rbf'],
-            'gamma': np.linspace(2 ** -15, 2 ** 3, 4)
-        }
-        model_clf = svm.SVC()
-        grid = GridSearchCV(model_clf, param_grid, refit = True, verbose = 3)
-        return grid
-
     def train(self, model):
         """
         Trains the provided model using the training data.
@@ -125,7 +133,7 @@ class SVMClassifier:
         model.fit(self.X_train, self.y_train)
         return model
 
-    def evaluate(self, model):
+    def evaluate(self, model, y_pred):
         """
         Evaluates the provided model by calculating its accuracy on the test set.
 
@@ -135,8 +143,26 @@ class SVMClassifier:
         Returns:
             float: The accuracy of the model on the test set.
         """
-        accuracy = model.score(self.X_test, self.y_test)
-        return accuracy
+        print("Best parameters found: ", model.best_params_, "\n")
+        
+        accuracy = accuracy_score(self.y_test, y_pred)
+        print(f"Accuracy: {accuracy}", "\n")
+        
+        clf_report = classification_report(self.y_test, y_pred, output_dict=True)
+        clf_report_df = pd.DataFrame(clf_report).transpose()
+        print(f"Classification report: \n{clf_report_df}", "\n")
+        
+        conf_mat = confusion_matrix(self.y_test, y_pred,)
+        tn, fp, fn, tp = conf_mat.ravel()
+        
+        print("Confusion matrix:")
+        print(f"| TN | FP |   | {tn:2d} | {fp:2d} |")
+        print(f"|----|----| = |----|----|")
+        print(f"| FN | TP |   | {fn:2d} | {tp:2d} |")
+        
+        self.logger.log(model.best_params_, accuracy, conf_mat, clf_report_df)
+        
+        return accuracy, clf_report, conf_mat
 
     def predict(self, model):
         """
@@ -150,7 +176,7 @@ class SVMClassifier:
         """
         return model.predict(self.X_test)
 
-    def save_results(self, results):
+    def save_results(self, pred_results):
         """
         Saves the provided results to a CSV file in the specified output directory.
 
@@ -159,25 +185,25 @@ class SVMClassifier:
         """
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
+            
+        pred_results = pd.DataFrame(pred_results, columns=[self.target_value])
+        results = pd.concat([pd.DataFrame(self.data.drop(self.target_value, axis=1), columns=self.data.columns[:-1]), pred_results], axis=1)
+        
         results.to_csv(os.path.join(self.output_dir, 'results.csv'), index=False)
 
     def run(self):
         grid = self.tune_hyperparameter()
         model = self.train(grid)
-        print("Best parameters found: ", model.best_params_)
         
         best_model = model.best_estimator_
-        
         best_model = self.train(best_model)
-        
-        accuracy = self.evaluate(best_model)
-        print(f"Accuracy: {accuracy}")
         
         y_pred = self.predict(best_model)
         
-        y_pred = pd.DataFrame(y_pred, columns=[self.target_value])
-        results = pd.concat([pd.DataFrame(self.data.drop(self.target_value, axis=1), columns=self.data.columns[:-1]), y_pred], axis=1)
-        self.save_results(results)
+        self.evaluate(model, y_pred)
+        
+        self.save_results(y_pred)
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Classify by SVM')
